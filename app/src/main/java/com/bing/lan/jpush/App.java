19b9ea6c35d171a3d567bc7fbac3b876486427d4
dsj_lan_bing <lb@gdysj.com.cn>
2017-06-21 18:21:22 +0800

    first init

---
+++ app/src/main/java/com/bing/lan/jpush/App.java
@@ -0,0 +1,23 @@
+package com.bing.lan.jpush;
+
+import android.app.Application;
+
+import cn.jpush.android.api.JPushInterface;
+
+/**
+ * Created by 520 on 2017/6/21.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+
+
+    }
+}
