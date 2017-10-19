486aa6f4d15b6f626f40e1d4d5d60c89423b1148
dsj_lan_bing <lb@gdysj.com.cn>
2017-10-19 11:13:52 +0800

    umeng推送接入科大讯飞

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# JPushDemo
+#  PushDemo
+
+{
+    "body": {
+        "after_open": "go_app",
+        "play_lights": "true",
+        "play_sound": "true",
+        "play_vibrate": "true",
+        "text": "oppo推送测试",
+        "ticker": "oppo3",
+        "title": "oppo3"
+    },
+    "display_type": "notification",
+    "msg_id": "umii2fm150837827400910",
+    "random_min": 0
+}
+
+{
+    "action_type": 1,
+    "device_token": "AntT68OEimhiis2hykAA3JVdePJhAlw9EwG4gcoLft6_",
+    "header": {
+        "access": "Wi-Fi",
+        "access_subtype": "Unknown",
+        "app_version": "1.0",
+        "appkey": "59e7fff5f29d9867280001c4",
+        "carrier": "46011",
+        "channel": "Unknown",
+        "device_model": "OPPO R9s",
+        "din": "XLNrC2FS957c8QP4VJQgUA==",
+        "os": "Android",
+        "os_version": "6.0.1",
+        "sdk_type": "Android",
+        "sdk_version": "3.1.3",
+        "umid": "",
+        "version_code": "1"
+    },
+    "msg_id": "umntj7h150838089121210",
+    "ts": 1508380923265,
+    "utdid": "WNSeexti0T8DAIaPLFqeaQCE"
+}
+
+{"success":"ok","interval":0}
--- UPush/src/main/java/com/bing/lan/upush/App.java
+++ UPush/src/main/java/com/bing/lan/upush/App.java
@@ -1,10 +1,19 @@
 package com.bing.lan.upush;
 
 import android.app.Application;
+import android.app.Notification;
+import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.bing.lan.voice.AudioUtils;
+import com.iflytek.cloud.SpeechUtility;
 import com.umeng.message.IUmengRegisterCallback;
 import com.umeng.message.PushAgent;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
 
 /**
  * Created by 520 on 2017/6/21.
@@ -17,10 +26,62 @@ public class App extends Application {
         super.onCreate();
 
         initPush();
+
+        //接了科大讯飞 就无法收到推送 是 .so文件的问题
+        SpeechUtility.createUtility(getApplicationContext(), "appid=59e566ff");
+        AudioUtils.getInstance().init(getApplicationContext());
+
     }
 
     private void initPush() {
         PushAgent mPushAgent = PushAgent.getInstance(this);
+        //mPushAgent.setDebugMode(false);
+
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+            @Override
+            public void dealWithCustomAction(Context context, UMessage msg) {
+                Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+                Log.e("umeng onSuccess:", " oppo:  " + msg.custom);
+            }
+        };
+
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+            @Override
+            public Notification getNotification(Context context, UMessage msg) {
+                switch (msg.builder_id) {
+                    case 0:
+                        //Notification.Builder builder = new Notification.Builder(context);
+                        //RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
+                        //        R.layout.notification_view);
+                        //myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+                        //myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+                        //myNotificationView.setImageViewBitmap(R.id.notification_large_icon,
+                        //        getLargeIcon(context, msg));
+                        //myNotificationView.setImageViewResource(R.id.notification_small_icon,
+                        //        getSmallIconId(context, msg));
+                        //builder.setContent(myNotificationView)
+                        //        .setSmallIcon(getSmallIconId(context, msg))
+                        //        .setTicker(msg.ticker)
+                        //        .setAutoCancel(true);
+
+                        //return builder.getNotification();
+
+                        Log.e("getNotification:", "msg: " + msg.getRaw().toString());
+                        if (!TextUtils.isEmpty(msg.text)) {
+                            AudioUtils.getInstance().speakText(msg.text);
+                        }
+
+                        return null;
+                    default:
+                        //默认为0，若填写的builder_id并不存在，也使用默认。
+                        return super.getNotification(context, msg);
+                }
+            }
+        };
+        mPushAgent.setMessageHandler(messageHandler);
+
         //注册推送服务，每次调用register方法都会回调该接口
         mPushAgent.register(new IUmengRegisterCallback() {
 
